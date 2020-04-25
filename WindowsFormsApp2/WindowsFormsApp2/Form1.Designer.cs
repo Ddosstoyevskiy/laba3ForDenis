8e4ad3cb67fd6da330ba7673f94f72595a2cc45e
Ddosstoyevskiy <64312444+Ddosstoyevskiy@users.noreply.github.com>
2020-04-25 15:03:30 +0300

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(115, 461);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Window;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(138, 461);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Window;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(770, 462);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(188, 447);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(410, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Начать заново";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(647, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(177, 340);
+            this.listBox1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 463);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(97, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Игрок 1 ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(625, 460);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(116, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Игрок 2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Window;
+            this.label5.Location = new System.Drawing.Point(12, 443);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Ник 1 игрока:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Window;
+            this.label6.Location = new System.Drawing.Point(622, 440);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Ник 2 игрока:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Window;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(747, 460);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 23);
+            this.label3.TabIndex = 10;
+            this.label3.Text = ":";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.Window;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(700, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 19);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "История";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(848, 496);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Krest_noll";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        
+        int sum = 0;
+        int[ ,] win = { { 0, 0, 0}, { 0, 0, 0}, {0,0, 0} };
+        int[,] step = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+        int player1=0;
+        int player2 = 0;
+        int c = 0;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+        }
+
+        
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+            int x=0;
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            int xc = this.Width / 2;
+            int yc = this.Height / 2;
+            g.TranslateTransform(xc, yc);
+            
+
+
+
+            for (int i= 0; i<=3; i++)
+            {
+                g.DrawLine(new Pen(Color.Gray, 4.0f), -300+x, -200, -300+x, 100);
+                g.DrawLine(new Pen(Color.Gray, 4.0f), -300, -200+x, 0, -200+x);
+                x=x+100;
+            }
+            if (win[0, 0] == 1 & win[1, 0] == 1 & win[2, 0] == 1)
+            {
+                g.DrawLine(new Pen(Color.Black, 4.0f), -300, -200, -300 + x, 100);
+            }
+
+
+
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            int xc = this.Width / 2;
+            int yc = this.Height / 2;
+            g.TranslateTransform(xc, yc);
+            int x = e.X;
+            int y = e.Y;
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int q = 0; q < 3; q++)
+                {
+                    
+                    sum += step[q, i];
+                }
+                
+                
+
+            }
+            
+
+
+            int j = 0;
+            if (sum%2==0) {
+
+
+                for (int i = 0; i < 3; i++)
+                {
+
+                    if (y > 20 & y < 120 & x > 25 + j & x < 125 + j)
+                    {
+                        Krest(g, j, 0, j, 0);
+                        win[0+i,0] = 1;
+                        step[0 + i, 0] = 1;
+
+                    }
+                    if (y > 120 & y < 220 & x > 25 + j & x < 125 + j)
+                    {
+                        Krest(g, j, 100, j, 100);
+                        win[0 + i, 1] = 1;
+                        step[0 + i, 1] = 1;
+                    }
+                    if (y > 220 & y < 320 & x > 25 + j & x < 125 + j)
+                    {
+                        Krest(g, j, 200, j, 200);
+                        win[0 + i, 2] = 1;
+                        step[0 + i, 2] = 1;
+                    }
+                    j = j + 100;
+                }
+
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+
+                    if (y > 20 & y < 120 & x > 25 + j & x < 125 + j)
+                    {
+                        Zero(g, j, 0);
+                        win[0 + i, 0] = 2;
+                        step[0 + i, 0] = 1;
+                    }
+                    if (y > 120 & y < 220 & x > 25 + j & x < 125 + j)
+                    {
+                        Zero(g, j, 100);
+                        win[0 + i, 1] = 2;
+                        step[0 + i, 1] = 1;
+
+                    }
+                    if (y > 220 & y < 320 & x > 25 + j & x < 125 + j)
+                    {
+                        Zero(g, j, 200);
+                        win[0 + i, 2] = 2;
+                        step[0 + i, 2] = 1;
+                    }
+                    j = j + 100;
+                }
+            }
+
+            sum = 0;
+
+            if (win[0, 0] == 1 & win[1, 0] == 1 & win[2, 0] == 1) {
+                player1++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), -300, -150, 0, -150);
+                WinLose(g, textBox1.Text);
+                label2.Text = player1.ToString();
+            } else if (win[0, 1] == 1 & win[1, 1] == 1 & win[2, 1] == 1)
+            {
+                player1++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), -300, -50, 0, -50);
+                WinLose(g, textBox1.Text);
+                label2.Text = player1.ToString();
+            } else if (win[0, 2] == 1 & win[1, 2] == 1 & win[2, 2] == 1) {
+                player1++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), -300, 50, 0, 50);
+                WinLose(g, textBox1.Text);
+                label2.Text = player1.ToString();
+            } else if (win[0, 0] == 1 & win[0, 1] == 1 & win[0, 2] == 1) {
+                player1++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), -250, -200, -250, 100);
+                WinLose(g, textBox1.Text);
+                label2.Text = player1.ToString();
+            } else if (win[1, 0] == 1 & win[1, 1] == 1 & win[1, 2] == 1) {
+                player1++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), -150, -200, -150, 100);
+                WinLose(g, textBox1.Text);
+                label2.Text = player1.ToString();
+            } else if (win[0, 0] == 1 & win[1, 1] == 1 & win[2, 2] == 1) {
+                player1++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), -300, -200, 0, 100);
+                WinLose(g, textBox1.Text);
+                label2.Text = player1.ToString();
+            } else if (win[2,0]==1 & win[2,1]==1 & win[2,2]==1) {
+                player1++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), -50, -200, -50, 100);
+                WinLose(g, textBox1.Text);
+                label2.Text = player1.ToString();
+            } else if (win[2, 0] == 1 & win[1, 1] == 1 & win[0, 2] == 1) {
+                player1++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), 0, -200, -300, 100);
+                WinLose(g, textBox1.Text);
+                label2.Text = player1.ToString();
+            } else if (win[0, 0] == 2 & win[1, 0] == 2 & win[2, 0] == 2)
+            {
+                player2++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), -300, -150, 0, -150);
+                WinLose(g, textBox2.Text);
+                label4.Text = player2.ToString();
+            } else if (win[0, 1] == 2 & win[1, 1] == 2 & win[2, 1] == 2) {
+                player2++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), -300, -50, 0, -50);
+                WinLose(g, textBox2.Text);
+                label4.Text = player2.ToString();
+            } else if (win[0, 2] == 2 & win[1, 2] == 2 & win[2, 2] == 2) {
+                player2++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), -300, 50, 0, 50);
+                WinLose(g, textBox1.Text);
+                label4.Text = player2.ToString();
+            } else if (win[0, 0] == 2 & win[0, 1] == 2 & win[0, 2] == 2) {
+                player2++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), -250, -200, -250, 100);
+                WinLose(g, textBox2.Text);
+                label4.Text = player2.ToString();
+            } else if (win[1, 0] == 2 & win[1, 1] == 2 & win[1, 2] == 2) {
+                player2++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), -150, -200, -150, 100);
+                WinLose(g, textBox2.Text);
+                label4.Text = player2.ToString();
+            } else if (win[2, 0] == 2 & win[1, 2] == 2 & win[2, 2] == 2) {
+                player2++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), -50, -200, -50, 100);
+                WinLose(g, textBox2.Text);
+                label4.Text = player2.ToString();
+            } else if (win[0, 0] == 2 & win[1, 1] == 2 & win[2, 2] == 2)
+            {
+                player2++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), -300, -200, 0, 100);
+                WinLose(g, textBox2.Text);
+                label4.Text = player2.ToString();
+            } else if (win[2, 0] == 2 & win[1, 1] == 2 & win[0, 2] == 2) {
+                player2++;
+                g.DrawLine(new Pen(Color.Black, 3.0f), 0, -200, -300, 100);
+                WinLose(g, textBox2.Text);
+                label4.Text = player2.ToString();
+            } else if (win[0, 0] != 0 & win[1, 0] != 0 & win[2, 0] != 0 & win[0, 1] != 0 & win[1, 1] != 0 & win[2, 1] != 0 & win[0, 2] != 0 & win[1, 2] != 0 & win[2, 2] != 0)
+            {
+
+                x = 0;
+
+                DialogResult result = MessageBox.Show(
+                  "Ничья(((( Начать сначала?",
+                  "even score!!!",
+                  MessageBoxButtons.YesNo,
+                  MessageBoxIcon.Exclamation,
+                  MessageBoxDefaultButton.Button1,
+                  MessageBoxOptions.ServiceNotification);
+                if (result == DialogResult.Yes)
+                {
+                    x = 0;
+                    g.Clear(Color.White);
+                    for (int i = 0; i <= 3; i++)
+                    {
+
+                        g.DrawLine(new Pen(Color.Gray, 4.0f), -300 + x, -200, -300 + x, 100);
+                        g.DrawLine(new Pen(Color.Gray, 4.0f), -300, -200 + x, 0, -200 + x);
+                        x = x + 100;
+                        for (int t = 0; t < 3; t++)
+                        {
+                            for (int q = 0; q < 3; q++)
+                            {
+                                win[q, t] = 0;
+                                step[q, t] = 0;
+                            }
+                        }
+                    }
+                } else if (result == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+            
+           c++;
+
+            
+            
+
+        }
+        void Krest( Graphics g,int x1, int y1, int x2, int y2)
+        {
+            g.DrawLine(new Pen(Color.Red, 2.0f), -300+10+x1, -200+10+y1, -200-10+x2, -100-10+y2);
+            g.DrawLine(new Pen(Color.Red, 2.0f), -200-10+x1, -200+10+y1, -300+10+x2, -100-10+y2);
+
+
+        }
+        void Zero(Graphics g,int x1,int y1)
+        {
+            g.DrawEllipse(new Pen(Color.Blue, 3.0f),-200+x1,-100+y1,-100,-100);
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            label2.Text = "0";
+            label4.Text = "0";
+            listBox1.Items.Add($"{textBox1.Text}: {textBox2.Text}  | {player1}:{player2}");
+            player1 = 0;
+            player2 = 0;
+        }
+        void WinLose(Graphics g, String winner)   
+        {
+            int x = 0;
+            DialogResult result = MessageBox.Show(
+                  $"победитель {winner}!!!! Начать новую игру?",
+                  "Win!!!",
+                  MessageBoxButtons.YesNo,
+                  MessageBoxIcon.Exclamation,
+                  MessageBoxDefaultButton.Button1,
+                  MessageBoxOptions.ServiceNotification);
+            if (result == DialogResult.Yes)
+            {
+                
+                g.Clear(Color.White);
+                for (int i = 0; i <= 3; i++)
+                {
+
+                    g.DrawLine(new Pen(Color.Gray, 4.0f), -300 + x, -200, -300 + x, 100);
+                    g.DrawLine(new Pen(Color.Gray, 4.0f), -300, -200 + x, 0, -200 + x);
+                    x = x + 100;
+                    for (int t = 0; t < 3; t++)
+                    {
+                        for (int q = 0; q < 3; q++)
+                        {
+                            win[q, t] = 0;
+                            step[q, t] = 0;
+                        }
+                    }
+                }
+            }
+            else if (result == DialogResult.No)
+            {
+                this.Close();
+            }
+
+        }
+
+
+    }
+}
