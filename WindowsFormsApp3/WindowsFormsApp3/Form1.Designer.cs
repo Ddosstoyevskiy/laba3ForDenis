510a6e156da1a5794d38db445fba19777994ebb2
Ddosstoyevskiy <64312444+Ddosstoyevskiy@users.noreply.github.com>
2020-04-26 13:43:40 +0300

    laba_5

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(25, 45);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(227, 161);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Состояния счета";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Валюта:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Баланс:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(84, 84);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(128, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(25, 275);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(227, 161);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Транзакция";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(136, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Снять";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Зачислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Валюта:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Сумма:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(84, 84);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(128, 24);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(84, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(128, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox4);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Location = new System.Drawing.Point(304, 45);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(358, 161);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Курс валют по ЦБ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(202, 88);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(128, 22);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Курс евро в рублях:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(159, 17);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Курс долларав рублях:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(202, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(128, 22);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.Text = "0";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox6);
+            this.groupBox4.Controls.Add(this.comboBox5);
+            this.groupBox4.Controls.Add(this.comboBox4);
+            this.groupBox4.Controls.Add(this.textBox5);
+            this.groupBox4.Location = new System.Drawing.Point(304, 275);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(358, 161);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Конвертер валют";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(169, 86);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(161, 22);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.Text = "0";
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(6, 84);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 24);
+            this.comboBox5.TabIndex = 4;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(6, 42);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 24);
+            this.comboBox4.TabIndex = 3;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(169, 40);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(161, 22);
+            this.textBox5.TabIndex = 0;
+            this.textBox5.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(129, 475);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 26);
+            this.label7.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 531);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Банковский счет";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        double r = 0;
+        int q = 0;
+        int a = 0;
+        int v = 0;
+        public Form1()
+        {
+
+
+            
+            InitializeComponent();
+            textBox1.ReadOnly = true;
+            comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox1.Items.Add("USD");
+            comboBox1.Items.Add("RUB");
+            comboBox1.Items.Add("EUR");
+            comboBox2.Items.Add("USD");
+            comboBox2.Items.Add("RUB");
+            comboBox2.Items.Add("EUR");
+            comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox4.Items.Add("USD");
+            comboBox4.Items.Add("RUB");
+            comboBox4.Items.Add("EUR");
+            comboBox5.Items.Add("USD");
+            comboBox5.Items.Add("RUB");
+            comboBox5.Items.Add("EUR");
+            textBox3.ReadOnly = true;
+            textBox4.ReadOnly = true;
+            textBox5.ReadOnly = true;
+            WebClient client = new WebClient();
+            client.Encoding = System.Text.Encoding.UTF8;
+            string ss;
+            using (Stream stream = client.OpenRead("https://www.banki.ru/products/currency/usd/"))
+            {
+                using (StreamReader sr = new StreamReader(stream))
+                    ss = sr.ReadToEnd();
+            }
+            String Rate = Regex.Match(ss, @"<div class=""currency-table__large-text"">(\d+,\d+)</div>").Groups[1].Value;
+            textBox3.Text = Rate;
+          
+             
+            using (Stream stream = client.OpenRead("https://www.banki.ru/products/currency/eur/"))
+            {
+                using (StreamReader sr = new StreamReader(stream))
+                    ss = sr.ReadToEnd();
+            }
+            Rate = Regex.Match(ss, @"<div class=""currency-table__large-text"">(\d+,\d+)</div>").Groups[1].Value;
+            textBox4.Text = Rate;
+            
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == comboBox2.Text & comboBox1.Text =="USD")
+            {
+                String s = textBox2.Text;
+                a = a + Convert.ToInt32(s);
+                textBox1.Text = a.ToString();
+            }else if (comboBox1.Text == comboBox2.Text & comboBox1.Text == "RUB")
+            {
+                String s = textBox2.Text;
+                q = q + Convert.ToInt32(s);
+                textBox1.Text = q.ToString();
+            }else if (comboBox1.Text == comboBox2.Text & comboBox1.Text == "EUR")
+            {
+                String s = textBox2.Text;
+                v = v + Convert.ToInt32(s);
+                textBox1.Text = v.ToString();
+            }
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.Text == "USD")
+            {
+                textBox1.Text = a.ToString();
+            }else if (comboBox1.Text == "RUB")
+            {
+                textBox1.Text = q.ToString();
+            }
+            else if (comboBox1.Text == "EUR")
+            {
+                textBox1.Text = v.ToString();
+            }
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox4.Text == "USD")
+            {
+                textBox5.Text = a.ToString();
+            }
+            else if (comboBox4.Text == "RUB")
+            {
+                textBox5.Text = q.ToString();
+            }
+            else if (comboBox4.Text == "EUR")
+            {
+                textBox5.Text = v.ToString();
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            client.Encoding = System.Text.Encoding.UTF8;
+            string ss;
+            using (Stream stream = client.OpenRead("https://www.banki.ru/products/currency/usd/"))
+            {
+                using (StreamReader sr = new StreamReader(stream))
+                    ss = sr.ReadToEnd();
+            }
+            String Rate = Regex.Match(ss, @"<div class=""currency-table__large-text"">(\d+,\d+)</div>").Groups[1].Value;
+            double val = Convert.ToDouble(Rate);
+
+            if (textBox6.Text !="")
+            {
+                if (comboBox5.Text == "USD" & comboBox4.Text == "RUB")
+                {
+                    int t = Convert.ToInt32(textBox6.Text);
+                    r = r + t * val;
+                    textBox5.Text = r.ToString();
+                    r = 0;
+                } else if (comboBox5.Text == "RUB" & comboBox4.Text == "USD") 
+                {
+                    int t = Convert.ToInt32(textBox6.Text);
+                    r = r + t / val;
+                    textBox5.Text = r.ToString();
+                    r = 0;
+                }
+                using (Stream stream = client.OpenRead("https://www.banki.ru/products/currency/eur/"))
+                {
+                    using (StreamReader sr = new StreamReader(stream))
+                        ss = sr.ReadToEnd();
+                }
+                Rate = Regex.Match(ss, @"<div class=""currency-table__large-text"">(\d+,\d+)</div>").Groups[1].Value;
+                val = Convert.ToDouble(Rate);
+                if (comboBox5.Text == "EUR" & comboBox4.Text == "RUB")
+                {
+                    int t = Convert.ToInt32(textBox6.Text);
+                    r = r + t * val;
+                    textBox5.Text = r.ToString();
+                    r = 0;
+                }
+                else if (comboBox5.Text == "RUB" & comboBox4.Text == "EUR")
+                {
+                    int t = Convert.ToInt32(textBox6.Text);
+                    r = r + t / val;
+                    textBox5.Text = r.ToString();
+                    r = 0;
+                }
+                if (comboBox5.Text == "EUR" & comboBox4.Text == "USD")
+                {
+                    int t = Convert.ToInt32(textBox6.Text);
+                    r = r + t / 0.92;
+                    textBox5.Text = r.ToString();
+                    r = 0;
+                }
+                else if (comboBox5.Text == "USD" & comboBox4.Text == "EUR")
+                {
+                    int t = Convert.ToInt32(textBox6.Text);
+                    r = r + t * 0.92;
+                    textBox5.Text = r.ToString();
+                    r = 0;
+                }
+
+
+
+            }
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "0")
+            {
+                if (comboBox1.Text == comboBox2.Text & comboBox1.Text == "USD")
+                {
+                    String s = textBox2.Text;
+                    a = a - Convert.ToInt32(s);
+                    textBox1.Text = a.ToString();
+                }
+                else if (comboBox1.Text == comboBox2.Text & comboBox1.Text == "RUB")
+                {
+                    String s = textBox2.Text;
+                    q = q - Convert.ToInt32(s);
+                    textBox1.Text = q.ToString();
+                }
+                else if (comboBox1.Text == comboBox2.Text & comboBox1.Text == "EUR")
+                {
+                    String s = textBox2.Text;
+                    v = v - Convert.ToInt32(s);
+                    textBox1.Text = v.ToString();
+                }
+            }
+            
+        }
+    }
+}
